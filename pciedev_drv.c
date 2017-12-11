18e08e2c1a9ba5e58f65dc3ff6c3f7be3f76326b
Ludwig Petrosyan <ludwig.petrosyan@desy.de>
2017-12-11 09:17:09 +0100

    fixed for kernel >=4.13

--- pciedev_drv.c
+++ pciedev_drv.c
@@ -3,18 +3,18 @@
 *
 *This file is part of PCIEDEV driver.
 *
-*Foobar is free software: you can redistribute it and/or modify
+*PCIEDEV is free software: you can redistribute it and/or modify
 *it under the terms of the GNU General Public License as published by
 *the Free Software Foundation, either version 3 of the License, or
 *(at your option) any later version.
 *
-*Foobar is distributed in the hope that it will be useful,
+*PCIEDEV is distributed in the hope that it will be useful,
 *but WITHOUT ANY WARRANTY; without even the implied warranty of
 *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *GNU General Public License for more details.
 *
 *You should have received a copy of the GNU General Public License
-*along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*along with PCIEDEV.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
 
@@ -34,7 +34,7 @@
 
 MODULE_AUTHOR("Ludwig Petrosyan");
 MODULE_DESCRIPTION("DESY AMC-PCIE board driver");
-MODULE_VERSION("5.0.0");
+MODULE_VERSION("6.0.0");
 MODULE_LICENSE("Dual BSD/GPL");
 
 pciedev_cdev     *pciedev_cdev_m = 0;
